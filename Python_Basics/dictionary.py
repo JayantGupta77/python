b52de1007624013b65ae4c17753a94b663274fcc
JayantGupta77 <gjayant517@gmail.com>
2025-09-17 22:29:27 +0530

    python

---
+++ Python_Basics/dictionary.py
@@ -0,0 +1,7 @@
+capitals = {"USA": "Washington, D.C.",
+             "France": "Paris",
+               "Japan": "Tokyo",
+               "India": "New Delhi"}
+print(capitals["India"])
+
+print(capitals.get("Germany", "Not Found"))
