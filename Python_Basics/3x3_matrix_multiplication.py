6e7bc34e9f5b57282ec32fc8ae5e00840007bd5f
JayantGupta77 <gjayant517@gmail.com>
2025-09-26 10:18:31 +0530

    python

---
+++ Python_Basics/3x3_matrix_multiplication.py
@@ -0,0 +1,28 @@
+def matrix_chain_order(p):
+    n = len(p) - 1
+    m = [[0] * n for _ in range(n)]
+    s = [[0] * n for _ in range(n)]
+
+    for l in range(2, n + 1):
+        for i in range(n - l + 1):
+            j = i + l - 1
+            m[i][j] = float('inf')
+            for k in range(i, j):
+                cost = m[i][k] + m[k + 1][j] + p[i] * p[k + 1] * p[j + 1]
+                if cost < m[i][j]:
+                    m[i][j] = cost
+                    s[i][j] = k
+    return s
+
+def multiply_matrices(A, B):
+    rows_A, cols_A = len(A), len(A[0])
+    rows_B, cols_B = len(B), len(B[0])
+    if cols_A != rows_B:
+        raise ValueError("Incompatible dimensions for multiplication")
+
+    result = [[0] * cols_B for _ in range(rows_A)]
+    for i in range(rows_A):
+        for j in range(cols_B):
+            for k in range(cols_A):
+                result[i][j] += A[i][k] * B[k][j]
+    return result
