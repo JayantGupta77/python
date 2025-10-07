196c2b1e01a66e07e7dcf6c5d053fbabad387a83
JayantGupta77 <gjayant517@gmail.com>
2025-10-07 18:08:30 +0530

    python

---
+++ Python_Basics/sum_of_digits.py
@@ -0,0 +1,24 @@
+def sum_of_digits(number):
+    number = abs(number)
+    digit_sum = 0
+
+    while number > 0:
+        last_digit = number % 10
+        
+        digit_sum += last_digit
+        
+        #    e.g., 123 // 10 gives 12
+        number = number // 10
+        
+    return digit_sum
+
+num = 478
+result = sum_of_digits(num)
+
+print(f"The number is: {num}")
+print(f"The sum of its digits is: {result}") 
+
+num2 = 10001
+result2 = sum_of_digits(num2)
+print(f"\nThe number is: {num2}")
+print(f"The sum of its digits is: {result2}")
