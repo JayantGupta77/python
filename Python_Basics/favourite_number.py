e9545ce586d5465496b92f414812d80c0e597f11
JayantGupta77 <gjayant517@gmail.com>
2025-10-07 18:19:00 +0530

    python

---
+++ Python_Basics/favourite_number.py
@@ -0,0 +1,7 @@
+def print_favorite_numbers(numbers):
+    print("My favorite numbers are:")
+    for number in numbers:
+        print(f"- {number}")
+favorite_numbers = [7, 42, 108, 3, 16]
+
+print_favorite_numbers(favorite_numbers)
