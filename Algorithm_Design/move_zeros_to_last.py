60501c85993de91e1b7e5c48f9e4ca733654f293
JayantGupta77 <gjayant517@gmail.com>
2025-10-14 18:34:10 +0530

    python

---
+++ Algorithm_Design/move_zeros_to_last.py
@@ -0,0 +1,11 @@
+def push_zeros_to_end(arr):
+    count = 0  
+    n = len(arr)
+    for i in range(n):
+        if arr[i] != 0:
+            arr[count] = arr[i]
+            count += 1
+    while count < n:
+        arr[count] = 0
+        count += 1
+
