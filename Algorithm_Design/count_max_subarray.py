a7dfd3df83a90fb06f4801cd9e3fc888949daf94
JayantGupta77 <gjayant517@gmail.com>
2025-10-14 18:32:24 +0530

    python

---
+++ Algorithm_Design/count_max_subarray.py
@@ -0,0 +1,10 @@
+def max_subarray_sum(nums):
+    max_sum = float('-inf')  
+    curr_sum = 0
+    for num in nums:
+        curr_sum = max(num, curr_sum + num)
+        max_sum = max(max_sum, curr_sum)
+    return max_sum
+
+arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(max_subarray_sum(arr))  
