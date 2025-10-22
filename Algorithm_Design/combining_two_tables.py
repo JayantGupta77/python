9a71360a009726bdb0d3b379e342481e66f8721c
JayantGupta77 <gjayant517@gmail.com>
2025-10-22 17:50:20 +0530

    python

---
+++ Algorithm_Design/combining_two_tables.py
@@ -0,0 +1,7 @@
+def combine_tables(table1, table2):
+    return table1 + table2
+
+table1 = [(1, 'Alice', 25), (2, 'Bob', 30)]
+table2 = [(3, 'Charlie', 22), (4, 'David', 28)]
+
+print(combine_tables(table1, table2))
