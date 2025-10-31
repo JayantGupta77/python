32f0069c8535905e83debcaba9c87fcdccf44383
JayantGupta77 <gjayant517@gmail.com>
2025-10-31 09:23:12 +0530

    python

---
+++ Algorithm_Design/binary_tree.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+# Build the tree
+root = Node(10)
+root.left = Node(5)
+root.right = Node(15)
+root.left.left = Node(3)
+root.left.right = Node(7)
+root.right.right = Node(18)
+
+# Inorder traversal to verify
+def inorder(node):
+    if node:
+        inorder(node.left)
+        print(node.data, end=' ')
+        inorder(node.right)
+
+inorder(root)  # Output: 3 5 7 10 15 18
