6c628ce35b7719c7d9ed9116103bf80dd608b64a
JayantGupta77 <gjayant517@gmail.com>
2025-11-08 23:05:07 +0530

    python

---
+++ LeetCode/expensive_steps.py
@@ -0,0 +1,13 @@
+def minCostClimbingStairs(cost):
+    first = cost[0]
+    second = cost[1]
+    for i in range(2, len(cost)):
+        current = cost[i] + min(first, second)
+        first, second = second, current
+    return min(first, second)
+
+cost1 = [10, 15, 20]
+print(minCostClimbingStairs(cost1)) 
+
+cost2 = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+print(minCostClimbingStairs(cost2)) 
