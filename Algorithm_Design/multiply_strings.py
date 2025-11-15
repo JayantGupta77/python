36113193611bb73b7e4b33f6b3b7d0c102586c7a
JayantGupta77 <gjayant517@gmail.com>
2025-11-15 15:47:24 +0530

    python

---
+++ Algorithm_Design/multiply_strings.py
@@ -0,0 +1,21 @@
+def multiply_strings(num1: str, num2: str) -> str:
+    if num1 == "0" or num2 == "0":
+        return "0"
+
+    result = [0] * (len(num1) + len(num2))
+
+    num1, num2 = num1[::-1], num2[::-1]
+
+    for i in range(len(num1)):
+        for j in range(len(num2)):
+            digit_mul = int(num1[i]) * int(num2[j])
+            result[i + j] += digit_mul
+            result[i + j + 1] += result[i + j] // 10  # carry
+            result[i + j] %= 10
+
+    while result[-1] == 0:
+        result.pop()
+
+    return ''.join(map(str, result[::-1]))
+
+print(multiply_strings("123", "456"))  
