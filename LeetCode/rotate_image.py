df348cc18f17115de21f1a56fdcc1f64ef043932
JayantGupta77 <gjayant517@gmail.com>
2025-11-18 22:12:01 +0530

    python

---
+++ LeetCode/rotate_image.py
@@ -0,0 +1,20 @@
+def rotate_image(matrix):
+    n = len(matrix)
+    
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    
+    for row in matrix:
+        row.reverse()
+
+image = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+rotate_image(image)
+
+for row in image:
+    print(row)
