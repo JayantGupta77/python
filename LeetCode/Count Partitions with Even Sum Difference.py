040460f30414202cb2466e4caf3aa2f66105bc9c
JayantGupta77 <gjayant517@gmail.com>
2025-12-05 21:13:23 +0530

    py

---
+++ LeetCode/Count Partitions with Even Sum Difference.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def countPartitions(self, nums):
+
+        n = len(nums)
+        total_sum = sum(nums)
+        left_sum = 0
+        count = 0
+        
+        for i in range(n - 1):
+            left_sum += nums[i]
+            right_sum = total_sum - left_sum
+            
+            if (left_sum - right_sum) % 2 == 0:
+                count += 1
+        
+        return count
+        
