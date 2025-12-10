20d5264fc9509a82d0f6b1cf94796727ca3b5758
JayantGupta77 <gjayant517@gmail.com>
2025-12-11 00:20:23 +0530

    py

--- LeetCode/Count Square Sum Triples.py
+++ LeetCode/Count Square Sum Triples.py
@@ -7,7 +7,6 @@ class Solution(object):
         for a in range(1, n + 1):
             for b in range(1, n + 1):
                 c2 = a * a + b * b
-                # Check if c^2 is a perfect square within range
                 if c2 in squares:
                     c = int(c2 ** 0.5)
                     if c <= n:
