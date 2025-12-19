0e84e2562860535e50a6398dc7d3eef2ba0905ce
JayantGupta77 <gjayant517@gmail.com>
2025-12-19 23:43:28 +0530

    py

--- LeetCode/Binary Prefix Divisible By 5.py
+++ LeetCode/Binary Prefix Divisible By 5.py
@@ -2,7 +2,6 @@ class Solution(object):
     def prefixesDivBy5(self, nums):
         result = []
         current = 0
-        
         for bit in nums:
             current = (current * 2 + bit) % 5
             result.append(current == 0)
