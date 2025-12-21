0254951ae7d85d9ae0e121516397a127b1ad2192
JayantGupta77 <gjayant517@gmail.com>
2025-12-21 23:06:40 +0530

    py

--- LeetCode/Count_Odd_Numbers_in_Interval_Range.py
+++ LeetCode/Count_Odd_Numbers_in_Interval_Range.py
@@ -1,4 +1,3 @@
 class Solution(object):
-    def countOdds(self, low, high):
-        
+    def countOdds(self, low, high): 
         return ((high - low) // 2) + (1 if (low % 2 == 1 or high % 2 == 1) else 0)
