b3cb8b6d0ecc10211166ad30f91da228a52c6eff
JayantGupta77 <gjayant517@gmail.com>
2026-01-31 07:53:53 +0530

    py

--- LeetCode/length_of_longest_palendrome.py
+++ LeetCode/length_of_longest_palendrome.py
@@ -18,6 +18,6 @@ def longest_palindrome_length(s):
                     max_length = max(max_length, end - start + 1)
     return max_length
 
-# Example usage
+# Example usage:
 s = "abcbab"
 print(longest_palindrome_length(s))  # Output: 5
