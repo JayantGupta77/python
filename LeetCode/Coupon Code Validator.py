748ad353d6668f5aa7bda0400dbae287ef604668
JayantGupta77 <gjayant517@gmail.com>
2026-02-13 13:27:13 +0530

    py

--- LeetCode/Coupon Code Validator.py
+++ LeetCode/Coupon Code Validator.py
@@ -16,7 +16,6 @@ class Solution:
                     return False
             return True
 
-
         valid = []
         for i in range(n):
             if isActive[i] and is_valid_code(code[i]) and businessLine[i] in category_order:
